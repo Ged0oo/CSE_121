d4eb26922486a34ea71b3944e955a12021b51ba8
Mohamed Nagy <mohmmednagy@gmail.com>
2023-09-01 15:08:16 +0300

    Adding Essential Stack APIs

--- StackArray/StackArray.c
+++ StackArray/StackArray.c
@@ -1,1 +1,42 @@
 #include "StackArray.h"
+
+StackReturn_t StackInit(Stack_t* _stack)
+{
+	if (NULL == _stack)
+	{
+		return STACK_NULL;
+	}
+	else
+	{
+		_stack->top = -1;
+	}
+	
+}
+
+
+StackReturn_t StackFull(Stack_t* _stack)
+{
+	if (NULL == _stack)
+	{
+		return STACK_NULL;
+	}
+	else
+	{
+		if (_stack->top >= MaxStack)
+			return STACK_FULL;
+	}
+}
+
+
+StackReturn_t StackEmpty(Stack_t* _stack)
+{
+	if (NULL == _stack)
+	{
+		return STACK_NULL;
+	}
+	else
+	{
+		if (_stack->top == -1)
+			return STACK_EMPTY;
+	}
+}
--- StackArray/StackArray.h
+++ StackArray/StackArray.h
@@ -1,6 +1,28 @@
 #ifndef __STACK_ARRAY_H__
 #define __STACK_ARRAY_H__
 
+#define NULL			(void*)0
+#define MaxStack		100
 
+typedef void* StackEntry_t;
+
+typedef struct
+{
+	int top;
+	StackEntry_t entry[MaxStack];
+}Stack_t;
+
+typedef enum
+{
+	STACK_OK = 0,
+	STACK_NULL,
+	STACK_FULL,
+	STACK_EMPTY,
+	STACK_NOT_INTIALIZED
+}StackReturn_t;
+
+StackReturn_t StackInit(Stack_t* _stack);
+StackReturn_t StackFull(Stack_t* _stack);
+StackReturn_t StackEmpty(Stack_t* _stack);
 
 #endif // !__STACK_ARRAY_H__
