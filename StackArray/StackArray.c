16c124d268dc26ef6fb20c127a125bd8b2621784
Mohamed Nagy <mohmmednagy@gmail.com>
2023-09-03 09:20:20 +0300

    Immplementing Extra Accesing Mechanisms

--- StackArray/StackArray.c
+++ StackArray/StackArray.c
@@ -89,3 +89,82 @@ StackReturn_t StackPup(Stack_t* _stack, StackEntry_t* _entry)
 }
 
 
+StackReturn_t StackTop(Stack_t* _stack, StackEntry_t* _entry)
+{
+	if ((NULL == _stack) || (NULL == _entry))
+	{
+		return STACK_NULL;
+	}
+	else if (STACK_NOT_INTIALIZED == StackInit(_stack))
+	{
+		return STACK_NOT_INTIALIZED;
+	}
+	else if (STACK_EMPTY == StackFull(_stack))
+	{
+		return STACK_EMPTY;
+	}
+	else
+	{
+		*_entry = _stack->entry[-- _stack->top];
+		return STACK_OK;
+	}
+}
+
+
+StackReturn_t StackSize(Stack_t* _stack, int* _size)
+{
+	if (NULL == _stack)
+	{
+		return STACK_NULL;
+	}
+	else if (STACK_NOT_INTIALIZED == StackInit(_stack))
+	{
+		return STACK_NOT_INTIALIZED;
+	}
+	else
+	{
+		*_size = ++ _stack->top ;
+		return STACK_OK;
+	}
+}
+
+
+StackReturn_t StackClear(Stack_t* _stack)
+{
+	if (NULL == _stack)
+	{
+		return STACK_NULL;
+	}
+	else if (STACK_NOT_INTIALIZED == StackInit(_stack))
+	{
+		return STACK_NOT_INTIALIZED;
+	}
+	else
+	{
+		_stack->top = -1;
+		return STACK_OK;
+	}
+}
+
+
+StackReturn_t StackTraverse(Stack_t* _stack, void (*pvisit)(StackEntry_t))
+{
+	if (NULL == _stack)
+	{
+		return STACK_NULL;
+	}
+	else if (STACK_NOT_INTIALIZED == StackInit(_stack))
+	{
+		return STACK_NOT_INTIALIZED;
+	}
+	else if (STACK_EMPTY == StackFull(_stack))
+	{
+		return STACK_EMPTY;
+	}
+	else
+	{
+		for (int i = _stack->top; i >= 0; i--)
+			(*pvisit)(_stack->entry[i]);
+		return STACK_OK;
+	}
+}
