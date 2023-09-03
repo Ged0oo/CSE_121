358bf92da4cbdd80794e3633956d713121bb52fb
Mohamed Nagy <mohmmednagy@gmail.com>
2023-09-03 09:18:27 +0300

    Immplementing Push, Pup APIs

--- StackArray/StackArray.c
+++ StackArray/StackArray.c
@@ -42,4 +42,50 @@ StackReturn_t StackEmpty(Stack_t* _stack)
 }
 
 
+StackReturn_t StackPush(Stack_t* _stack, StackEntry_t  _entry)
+{
+	StackReturn_t stackRet = STACK_OK;
+	if(NULL == _stack)
+	{
+		return STACK_NULL;
+	}
+	else if (STACK_NOT_INTIALIZED == StackInit(_stack))
+	{
+		return STACK_NOT_INTIALIZED;
+	}
+	else if (STACK_FULL == StackFull(_stack))
+	{
+		return STACK_FULL;
+	}
+	else 
+	{	
+		_stack->top++;
+		_stack->entry[_stack->top] = _entry;	
+		return STACK_OK;
+	}
+}
+
+
+StackReturn_t StackPup(Stack_t* _stack, StackEntry_t* _entry)
+{
+	if ((NULL == _stack) || (NULL == _entry))
+	{
+		return STACK_NULL;
+	}
+	else if (STACK_NOT_INTIALIZED == StackInit(_stack))
+	{
+		return STACK_NOT_INTIALIZED;
+	}
+	else if (STACK_EMPTY == StackFull(_stack))
+	{
+		return STACK_EMPTY;
+	}
+	else
+	{
+		_stack->top--;
+		*_entry = _stack->entry[_stack->top];
+		return STACK_OK;
+	}
+}
+
 
