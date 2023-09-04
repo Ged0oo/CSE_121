d1d1847fcaaba0a2e8fb95d970553c000a766a3f
Mohamed Nagy <mohmmednagy@gmail.com>
2023-09-04 21:41:33 +0300

    Adding linked Stack Main Immplementation

---
+++ StackLinked/StackLinked.c
@@ -0,0 +1,61 @@
+#include "StackLinked.h"
+
+
+StackReturn_t StackLinkedInit(StackLinked_t* _stack)
+{
+
+}
+
+
+StackReturn_t StackLinkedFull(StackLinked_t* _stack)
+{
+
+}
+
+
+StackReturn_t StackLinkedEmpty(StackLinked_t* _stack)
+{
+
+}
+
+
+StackReturn_t StackLinkedPush(StackLinked_t* _stack, StackEntry_t  _entry)
+{
+
+}
+
+
+StackReturn_t StackLinkedPup(StackLinked_t* _stack, StackEntry_t* _entry)
+{
+
+}
+
+
+StackReturn_t StackLinkedTop(StackLinked_t* _stack, StackEntry_t* _entry)
+{
+
+}
+
+
+StackReturn_t StackLinkedSize(StackLinked_t* _stack, int* _size)
+{
+
+}
+
+
+StackReturn_t StackLinkedClear(StackLinked_t* _stack)
+{
+
+}
+
+
+StackReturn_t StackLinkedDisplay(StackLinked_t* _stack)
+{
+
+}
+
+
+StackReturn_t StackLinkedTraverse(StackLinked_t* _stack, void (*pvisit)(StackEntry_t))
+{
+
+}
--- main.c
+++ main.c
@@ -1,30 +1,8 @@
 #include <stdio.h>
 #include "StackArray/StackArray.h"
 
-void disStack(StackEntry_t st)
-{
-	printf(" %s ", st);
-}
 
 void main(void)
 {
-	Stack_t stack;
-	StackReturn_t ret;
-	StackEntry_t st = NULL;
-
-	ret = StackArrayInit(&stack);
-	ret = StackArrayEmpty(&stack);
-	
-	ret = StackArrayPush(&stack, "Mabrock");
-	ret = StackArrayPush(&stack, "Nagy");
-	ret = StackArrayPush(&stack, "Mohamed");
-
-	ret = StackArrayEmpty(&stack);
-
-	ret = StackArrayTraverse(&stack, disStack);
-	printf("\n");
-
-	ret = StackArrayPup(&stack, &st);
-
-	ret = StackArrayDisplay(&stack);
+	printf("\nHello_World\n");
 }
