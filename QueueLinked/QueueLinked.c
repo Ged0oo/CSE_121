29c2ceca6d46b83fc934f2528a84937fab325348
Mohamed Nagy <mohmmednagy@gmail.com>
2023-09-08 17:02:45 +0300

    Adding Linked Queue APIs Prototypes

--- QueueLinked/QueueLinked.c
+++ QueueLinked/QueueLinked.c
@@ -1,1 +1,60 @@
 #include "QueueLinked.h"
+
+QueueLinkedReturn_t QueueLinkedInit(QueueLinked_t* _queue)
+{
+
+}
+
+
+QueueLinkedReturn_t QueueLinkedFull(QueueLinked_t* _queue)
+{
+
+}
+
+
+QueueLinkedReturn_t QueueLinkedEmpty(QueueLinked_t* _queue)
+{
+
+}
+
+
+QueueLinkedReturn_t QueueLinkedAppend(QueueLinked_t* _queue, QueueEntry_t  _entry)
+{
+
+}
+
+
+QueueLinkedReturn_t QueueLinkedServe(QueueLinked_t* _queue, QueueEntry_t* _entry)
+{
+
+}
+
+
+QueueLinkedReturn_t QueueLinkedTop(QueueLinked_t* _queue, QueueEntry_t* _entry)
+{
+
+}
+
+
+QueueLinkedReturn_t QueueLinkedSize(QueueLinked_t* _queue, int* _size)
+{
+
+}
+
+
+QueueLinkedReturn_t QueueLinkedClear(QueueLinked_t* _queue)
+{
+
+}
+
+
+QueueLinkedReturn_t QueueLinkedDisplay(QueueLinked_t* _queue)
+{
+
+}
+
+
+QueueLinkedReturn_t QueueLinkedTraverse(QueueLinked_t* _queue, void (*pvisit)(QueueEntry_t))
+{
+
+}
