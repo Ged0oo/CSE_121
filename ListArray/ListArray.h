24a9befd7b059712689d17cc5574245a0c2459e0
Mohamed Nagy <mohmmednagy@gmail.com>
2023-09-15 10:51:24 +0300

    Init List Based on array directory

---
+++ ListArray/ListArray.h
@@ -0,0 +1,8 @@
+#ifndef __LIST_ARRAY_H__
+#define __LIST_ARRAY_H__
+
+
+
+
+
+#endif // !__LIST_ARRAY_H__
--- main.c
+++ main.c
@@ -1,24 +1,7 @@
 #include <stdio.h>
-#include "QueueLinked/QueueLinked.h"
 
 int main()
 {
-    QueueLinked_t queue;
-    QueueLinkedReturn_t ret = QUEUE_LINKED_OK;
-    QueueEntry_t qu ;
-    int size=0;
-
-    ret = QueueLinkedInit(&queue);
-    ret = QueueLinkedEmpty(&queue);
-
-    ret = QueueLinkedAppend(&queue, 3);
-    ret = QueueLinkedAppend(&queue, 8);
-    ret = QueueLinkedAppend(&queue, 6);
-    ret = QueueLinkedAppend(&queue, 8);
-    ret = QueueLinkedAppend(&queue, 4);
-
-   ret = QueueLinkedDisplay(&queue);
-
     printf("\nHello World\n");
     return 0;
 }
