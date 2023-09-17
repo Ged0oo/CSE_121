87755bbd7b618dcd6025fb330b8693310172942e
Mohamed Nagy <mohmmednagy@gmail.com>
2023-09-17 11:31:43 +0300

    Defining list linked Directory

---
+++ ListLinked/ListLinked.h
@@ -0,0 +1,6 @@
+#ifndef __LIST_LINKED_H__
+#define __LIST_LINKED_H__
+
+
+
+#endif // !__LIST_LINKED_H__
--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include "ListArray/ListArray.h"
 
 int main()
 {
+    List_t list;
+    ListEntry_t entry;
+    ListReturn_t ret = LIST_OK;
+
+    ret = ListArrayInit(&list);
+    ret = ListArrayFull(&list);
+    ret = ListArrayEmpty(&list);
+
+    ret = ListArrayInsert(&list, 6, 0);
+    ret = ListArrayInsert(&list, 1, 1);
+    ret = ListArrayInsert(&list, 5, 2);
+    ret = ListArrayInsert(&list, 9, 3);
+    ret = ListArrayInsert(&list, 7, 4);
+
     printf("\nHello World\n");
     return 0;
 }
