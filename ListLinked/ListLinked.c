3be59a2625c79aed962e276cd0ca7104fbd1887d
Mohamed Nagy <mohmmednagy@gmail.com>
2023-09-17 12:33:40 +0300

    Defining list linked APIs and Data types

--- ListLinked/ListLinked.c
+++ ListLinked/ListLinked.c
@@ -1,1 +1,66 @@
 #include "ListLinked.h"
+
+ListLinkedReturn_t ListLinkedInit(ListLinked_t* _list)
+{
+
+}
+
+
+ListLinkedReturn_t ListLinkedFull(ListLinked_t* _list)
+{
+
+}
+
+
+ListLinkedReturn_t ListLinkedEmpty(ListLinked_t* _list)
+{
+
+}
+
+
+ListLinkedReturn_t ListLinkedInsert(ListLinked_t* _list, ListLinkedEntry_t  _entry, int _index)
+{
+
+}
+
+
+ListLinkedReturn_t ListLinkedRetrieve(ListLinked_t* _list, ListLinkedEntry_t* _entry, int _index)
+{
+
+}
+
+
+ListLinkedReturn_t ListLinkedDelete(ListLinked_t* _list, ListLinkedEntry_t* _entry, int _index)
+{
+
+}
+
+
+ListLinkedReturn_t ListLinkedReplace(ListLinked_t* _list, ListLinkedEntry_t* _entry, int _index)
+{
+
+}
+
+
+ListLinkedReturn_t ListLinkedSize(ListLinked_t* _list, int* _size)
+{
+
+}
+
+
+ListLinkedReturn_t ListLinkedClear(ListLinked_t* _list)
+{
+
+}
+
+
+ListLinkedReturn_t ListLinkedDisplay(ListLinked_t* _list)
+{
+
+}
+
+
+ListLinkedReturn_t ListLinkedTraverse(ListLinked_t* _list, void (*pvisit)(ListLinkedEntry_t))
+{
+
+}
